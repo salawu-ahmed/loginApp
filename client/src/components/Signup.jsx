b705bb548e7fd1f486faf391a9b1b5a579fb396f
ahmed <a.salawu@outlook.com>
2024-05-14 14:57:50 +0000

    signup post successfull

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,21 +1,42 @@
 import React, { useState } from 'react'
-
+import axios from 'axios'
 
 const Signup = () => {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
+    const [name, setName] = useState('')
     const [password, setPassword] = useState('')
-    console.log(password, firstName, lastName);
 
-    const handleSubmit = () => {
-        console.log("Details submitted successfully");
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const user = {name, password}
+        try {
+            const res = await fetch('http://localhost:8000/app/user/signup', {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(user),
+            })
+            // const res = await axios.post('http://localhost:8000/app/user/signup', user)
+            if(!res.ok){
+                throw new Error("Didn't send")
+                return
+            }
+            const data = await res.json()
+            console.log(data);
+            console.log("Details submitted successfully");
+        }catch(error){
+            console.log(error);
+        }
     }
     return (
         <form onSubmit={handleSubmit}>
             <div>
-                <label htmlFor="firstName">First Name</label><input name="firstName" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} className='border border-black '/></div>
-            <div><label htmlFor="lastName">Last Name</label><input name="lastName" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}/></div>
-            <div><label htmlFor="password">Password</label><input name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/></div>
+                <label htmlFor="firstName">First Name</label><input name="name" type="text" value={name} onChange={(e) => setName(e.target.value)} className='border border-black ' />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label><input name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
             <div><button type="submit"></button></div>
         </form>
     )
